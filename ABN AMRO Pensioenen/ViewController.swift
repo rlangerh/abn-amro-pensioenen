22b949fbce977109ecdf45f1d78efb1bbb1080f9
Ronald Langerhorst <ronaldlangerhorst@macbook-pro-van-ronald.local>
2018-11-08 10:49:55 +0100

    Initial Commit

---
+++ ABN AMRO Pensioenen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABN AMRO Pensioenen
+//
+//  Created by Ronald Langerhorst on 08/11/2018.
+//  Copyright © 2018 ABN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
